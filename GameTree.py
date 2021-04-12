6e1655ada2393a773f94215aae0b7211fc12e273
Mark Bedaywi <50279151+Supermac30@users.noreply.github.com>
2021-04-12 19:22:39 -0400

    Delete GameTree.py

--- GameTree.py
+++
@@ -1,7 +0,0 @@
-""" Stores the Game Tree for any Game
-
-This file is Copyright (c) 2020 Mark Bedaywi
-"""
-from __future__ import annotations
-from typing import Optional
-from Game import GameState, MoveNotLegalError
